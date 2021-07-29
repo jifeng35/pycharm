7afe6fd3fa2e2e803556c5ad4c4da8945c764aaf
jifeng35 <84109179+jifeng35@users.noreply.github.com>
2021-07-29 10:31:14 +0800

    Default Changelist

---
+++ list.py
@@ -0,0 +1,9 @@
+my_list = [1, 'a', 2.3875, "小李"]
+print(my_list)
+print(my_list[3], "输出末尾元素的两种方式", my_list[-1])
+# -1是最后一个 0是第一个元素
+print(type(my_list[0]))
+print(type(my_list[1]))
+print(type(my_list[2]))
+print(type(my_list[3]))
+# 列表中存储的元素为定义时的数据类型
