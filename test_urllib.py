60331947a6d545cc51e4663f382c8131c196e22a
jifeng35 <84109179+jifeng35@users.noreply.github.com>
2021-08-04 16:33:57 +0800

    Default Changelist

--- test_urllib.py
+++ test_urllib.py
@@ -1,6 +1,11 @@
 import urllib.request as req
-content = req.urlopen("http://www.baidu.com")
-f = open("my 百度.html", "w")
-# print(content.read().decode('utf-8'))
-f.write(content.read().decode('utf-8'))
-f.close()
+# content = req.urlopen("http://www.baidu.com")
+# f = open("my 百度.html", "w")
+# # print(content.read().decode('utf-8'))
+# f.write(content.read().decode('utf-8'))
+# f.close()
+# post方式获取数0据
+import urllib.parse as par
+data = bytes(par.urlencode({"hello": "world"}), encoding="utf-8")
+content = req.urlopen("http://httpbin.org/post", data=data)
+print(content.read().decode("utf-8"))
