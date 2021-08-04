6709e54b97a51e260bb4fad7ad9f3bf5889f03dc
jifeng35 <84109179+jifeng35@users.noreply.github.com>
2021-08-04 16:24:22 +0800

    Default Changelist

--- test_urllib.py
+++ test_urllib.py
@@ -1,4 +1,6 @@
 import urllib.request as req
 content = req.urlopen("http://www.baidu.com")
 f = open("my 百度.html", "w")
-f.write(content.read().decode("utf-8"))
+# print(content.read().decode('utf-8'))
+f.write(content.read().decode('utf-8'))
+f.close()
