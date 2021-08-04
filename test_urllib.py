796fc8c9714f7562542273d18b56954bf54db7e5
jifeng35 <84109179+jifeng35@users.noreply.github.com>
2021-08-04 16:52:57 +0800

    Default Changelist

--- test_urllib.py
+++ test_urllib.py
@@ -6,6 +6,17 @@ import urllib.request as req
 # f.close()
 # post方式获取数0据
 import urllib.parse as par
+import urllib.error as err
 data = bytes(par.urlencode({"hello": "world"}), encoding="utf-8")
-content = req.urlopen("http://httpbin.org/post", data=data)
-print(content.read().decode("utf-8"))
+try:
+    content = req.urlopen("http://httpbin.org/post", data=data, timeout=3)  # data是发送的数据,timeout是最大响应时长
+    print(content.read().decode("utf-8"))
+    print("状态码为:", content.status)
+    print(content.getheader("Date"))
+    print(content.getheaders())
+except err.URLError as e:
+    print("Time out!\n", e)
+
+
+# Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)
+# Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.62
